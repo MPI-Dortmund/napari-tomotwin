8241065faafe074a81cbce0c3398721b503d91aa
thorstenwagner <twa1@posteo.de>
2024-02-29 11:02:48 +0100

    recalculate umap for specific cluster is working

--- src/napari_tomotwin/cluster_widget.py
+++ src/napari_tomotwin/cluster_widget.py
@@ -1,5 +1,6 @@
 import numpy as np
 from PyQt5.QtWidgets import QComboBox, QStyledItemDelegate, QFrame
+from napari.utils import notifications
 from qtpy.QtWidgets import (
     QFormLayout,
     QPushButton,
@@ -18,8 +19,18 @@ from qtpy.QtWidgets import (
 from qtpy.QtWidgets import QApplication
 from qtpy.QtGui import QColor, QPixmap
 from qtpy.QtCore import Qt
+from qtpy.QtCore import Signal
+import tempfile
+from napari_tomotwin.load_umap import LoadUmapTool
+
+
 from napari_clusters_plotter._plotter import PlotterWidget
 from napari_clusters_plotter._utilities import get_nice_colormap
+import pandas as pd
+from concurrent import futures
+from . import umap_refiner as urefine
+import os
+from napari_tomotwin._qt.labeled_progress_bar import LabeledProgressBar
 
 class ColorItemDelegate(QStyledItemDelegate):
     def paint(self, painter, option, index):
@@ -44,11 +55,15 @@ class ColorComboBox(QComboBox):
         self.setView(QListView(self))
 
 class ClusteringWidgetQt(QWidget):
+    refinement_done = Signal("PyQt_PyObject")
+
     def __init__(self, napari_viewer: "napari.Viewer"):
         super().__init__()
 
         self.viewer = napari_viewer
         self.plotter_widget: PlotterWidget
+        self.load_umap_tool: LoadUmapTool
+        self.progressbar = LabeledProgressBar(QLabel(""))
 
 
         #######
@@ -63,12 +78,14 @@ class ClusteringWidgetQt(QWidget):
         ####
         recalc_layout = QHBoxLayout()
         self._recalc_umap = QPushButton("Recalculate UMAP", self)
-        self._color_dropdown = self.get_current_cluster_dropdown()
+        self._recalc_umap.clicked.connect(self._on_refine_click)
+        self.refinement_done.connect(self.show_umap_callback)
+        self._cluster_dropdown = self.get_current_cluster_dropdown()
         self._show_targets = QPushButton("Show targets", self)
 
 
 
-        recalc_layout.addWidget(self._color_dropdown)
+        recalc_layout.addWidget(self._cluster_dropdown)
         recalc_layout.addWidget(self._show_targets)
         recalc_layout.addWidget(self._recalc_umap)
 
@@ -111,6 +128,13 @@ class ClusteringWidgetQt(QWidget):
         save_delete_layout.addWidget(self.delete)
         save_delete_layout.addWidget(self.save)
         self.layout().addRow("", save_delete_layout)
+        self.layout().addWidget(self.progressbar)
+
+    def set_plotter_widget(self, plotter_widget: PlotterWidget):
+        self.plotter_widget = plotter_widget
+
+    def set_umap_tool(self, umap_tool: LoadUmapTool):
+        self.load_umap_tool = umap_tool
 
     def add_candidate(self):
 
@@ -132,6 +156,25 @@ class ClusteringWidgetQt(QWidget):
 
             self.tableWidget.setItem(current_row_count, col, item)
 
+    def napari_update_umap(self, umap_embeddings, used_embeddings):
+
+        self.tmp_dir_path = tempfile.mkdtemp()
+        tmp_embed_pth = os.path.join(self.tmp_dir_path, next(tempfile._get_candidate_names()))
+        used_embeddings.to_pickle(tmp_embed_pth)
+        umap_embeddings.attrs['embeddings_path'] = tmp_embed_pth
+        tmp_umap_pth = os.path.join(self.tmp_dir_path, next(tempfile._get_candidate_names()))
+        umap_embeddings.to_pickle(tmp_umap_pth)
+
+        # Visualizse it
+        self.load_umap_tool.set_new_label_layer_name("UMAP Refined")
+        worker = self.load_umap_tool.start_umap_worker(tmp_umap_pth)
+        worker.start()
+
+    def show_umap_callback(self, future: futures.Future):
+        (umap_embeddings, used_embeddings) = future.result()
+        self.viewer.window._qt_window.setEnabled(True)
+        self.napari_update_umap(umap_embeddings, used_embeddings)
+
     @staticmethod
     def index_to_rgba(index: int) -> list[int]:
         colors = get_nice_colormap()
@@ -145,7 +188,7 @@ class ClusteringWidgetQt(QWidget):
         return rgba
     def update_all(self):
 
-        self.update_items_cluster_dropdown(self._color_dropdown, self.plotter_widget.layer_select.value.features['MANUAL_CLUSTER_ID'])
+        self.update_items_cluster_dropdown(self._cluster_dropdown, self.plotter_widget.layer_select.value.features['MANUAL_CLUSTER_ID'])
         self.update_items_cluster_dropdown(self._candidate_dropdown,
                                            self.plotter_widget.layer_select.value.features['MANUAL_CLUSTER_ID'])
 
@@ -161,10 +204,11 @@ class ClusteringWidgetQt(QWidget):
             pixmap.fill(QColor(*rgba))
             dropdown.setItemData(dropdown.count()-1, pixmap, Qt.DecorationRole)
 
+    def _on_refine_click(self):
+        self.viewer.window._qt_window.setEnabled(False)
+        #self.delete_points_layer()
+        self.reestimate_umap()
 
-
-    def set_plotter_widget(self, plotter_widget: PlotterWidget):
-        self.plotter_widget = plotter_widget
 
     def get_current_cluster_dropdown(self):
         color_dropdown = QComboBox(self)
@@ -186,3 +230,54 @@ class ClusteringWidgetQt(QWidget):
         pixmap.fill(QColor(Qt.red))
         color_dropdown.setItemData(2, pixmap, Qt.DecorationRole)
         return color_dropdown
+
+    def reestimate_umap(self):
+        try:
+            print("Read clusters")
+            clusters = self.plotter_widget.layer_select.value.features['MANUAL_CLUSTER_ID']
+            if not np.any(clusters>0):
+                raise KeyError
+        except KeyError:
+            notifications.show_info(f"No cluster selected. Can't refine.")
+            return
+
+        def get_embedding_path(pth: str) -> str:
+            '''
+            Checks if the embedding path exists. If it does not exist, it opens a file selection dialogue. Otherwise it returns the path.
+            '''
+            if not os.path.exists(pth):
+                msg = QMessageBox()
+                msg.setIcon(QMessageBox.Critical)
+                msg.setWindowTitle("Can't open embedding file")
+                msg.setText("Can't open embedding file")
+                msg.setInformativeText("Embedding path in metadata data (see below) does not exist or can't be accesst. Please click OK and select the path to the embedding file.")
+                msg.setDetailedText(pth)
+                msg.setStandardButtons(QMessageBox.Ok)
+                msg.exec_()
+                pth = QFileDialog.getOpenFileName(self, 'Open embedding file',
+                                                    os.getcwd(),
+                                                    "Embedding file (*.temb)")[0]
+
+            return pth
+
+        print("Read embeddings")
+        emb_pth = get_embedding_path(self.plotter_widget.layer_select.value.metadata['tomotwin']['embeddings_path'])
+
+
+        if emb_pth == "":
+            print("No path selected.")
+            return
+
+        self.plotter_widget.layer_select.value.metadata['tomotwin']['embeddings_path'] = emb_pth
+        embeddings = pd.read_pickle(emb_pth)
+
+        self.progressbar.setHidden(False)
+        self.progressbar.set_label_text("Recalculate umap")
+
+        # this workaround using signal is necessary, as "add_done_callback" starts the method
+        # in a separate thread, but to change Qt elements, it must be run in the same thread as the main program.
+        ppe = futures.ProcessPoolExecutor(max_workers=1)
+        target_cluster = self._cluster_dropdown.currentIndex()+1
+        f= ppe.submit(urefine.refine, clusters,embeddings, target_cluster)
+        ppe.shutdown(wait=False)
+        f.add_done_callback(self.refinement_done.emit)
--- src/napari_tomotwin/load_umap_widget.py
+++ src/napari_tomotwin/load_umap_widget.py
@@ -5,10 +5,7 @@ import os
 import os.path
 import shutil
 import tempfile
-import typing
 from concurrent import futures
-
-
 import numpy as np
 import pandas as pd
 from napari.utils import notifications
@@ -16,8 +13,8 @@ from napari_clusters_plotter._plotter import PlotterWidget
 from napari_clusters_plotter._utilities import get_nice_colormap
 from napari_tomotwin.make_targets_widget import _make_targets, _get_medoid_embedding
 from napari_tomotwin.load_umap import LoadUmapTool
-from numpy.typing import ArrayLike
 from qtpy.QtWidgets import QApplication
+from . import umap_refiner as urefine
 
 
 from qtpy.QtWidgets import (
@@ -31,77 +28,8 @@ from qtpy.QtWidgets import (
     QLineEdit
 )
 from qtpy.QtCore import Signal
-from tqdm import tqdm
 from napari_tomotwin._qt.labeled_progress_bar import LabeledProgressBar
 
-
-class UmapRefiner:
-
-    @staticmethod
-    def calculate_umap(
-            embeddings: pd.DataFrame,
-            transform_chunk_size: int = 400000,
-            reducer: "cuml.UMAP" = None,
-            ncomponents=2,
-            neighbors: int = 200,
-            metric: str = "euclidean") -> typing.Tuple[ArrayLike, "cuml.UMAP"]:
-
-        try:
-            # Import has to be happen here, as otherwise cuml is not working from a seperate process
-            # See: https://github.com/explosion/spaCy/issues/5507
-
-            import cuml
-            import cudf
-        except ImportError:
-            print("cuml can't be loaded")
-        print("Prepare data")
-        all_data = embeddings.drop(['filepath', 'Z', 'Y', 'X'], axis=1, errors='ignore')
-        indicis = np.arange(start=0, stop=len(all_data), dtype=int)
-        np.random.shuffle(indicis)
-        umap_embeddings = np.zeros(shape=(len(all_data),ncomponents))
-        num_chunks = max(1, int(math.ceil(len(indicis) / transform_chunk_size)))
-        print(
-            f"Transform complete dataset in {num_chunks} chunks with a chunksize of ~{int(len(indicis) / num_chunks)}")
-
-        for chunk in tqdm(np.array_split(indicis, num_chunks), desc="Transform"):
-            c = all_data.iloc[chunk]
-            if reducer is None:
-                reducer = cuml.UMAP(
-                    n_neighbors=neighbors,
-                    n_components=ncomponents,
-                    n_epochs=None,  # means automatic selection
-                    min_dist=0.0,
-                    random_state=19,
-                    metric=metric
-                )
-                print(f" Fit umap on {len(chunk)} samples")
-                umap_embeddings[chunk]= reducer.fit_transform(c)
-            else:
-                print(f" Fit umap on {len(chunk)} samples")
-                umap_embeddings[chunk] = reducer.transform(c)
-        del cuml
-        return umap_embeddings, reducer
-
-
-
-    @staticmethod
-    def refine(clusters, embeddings: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
-        embeddings = embeddings.drop(columns=["level_0", "index"], errors="ignore")
-        clmask = (clusters > 0).to_numpy().squeeze()
-        input_embeddings = embeddings.iloc[clmask, :]
-        umap_embedding_np, _ = UmapRefiner.calculate_umap(input_embeddings)
-
-
-        df_embeddings = pd.DataFrame(umap_embedding_np)
-        df_embeddings.reset_index(drop=True, inplace=True)
-        df_embeddings.columns = [f"umap_{i}" for i in range(umap_embedding_np.shape[1])]
-
-
-        input_embeddings.reset_index(drop=True, inplace=True)
-        df_embeddings = pd.concat([input_embeddings[['X', 'Y', 'Z']], df_embeddings], axis=1)
-        df_embeddings.attrs['embeddings_attrs'] = embeddings.attrs
-
-        return df_embeddings, input_embeddings
 
 
 class UmapToolQt(QWidget):
@@ -179,15 +107,14 @@ class UmapToolQt(QWidget):
                                                                                widget_name='Plotter Widget',
                                                                                tabify=False)
 
+            self.load_umap_tool = LoadUmapTool(pbar=self.progressBar, plotter_widget=self.plotter_widget)
+
             self.cluster_widget_dock, self.cluster_widget = self.viewer.window.add_plugin_dock_widget(
                 'napari-tomotwin',
                 widget_name='ClusterTool',
                 tabify=True)
             self.cluster_widget.set_plotter_widget(self.plotter_widget)
-
-
-
-            self.load_umap_tool = LoadUmapTool(pbar=self.progressBar, plotter_widget=self.plotter_widget)
+            self.cluster_widget.set_umap_tool(self.load_umap_tool)
 
             self.progressBar.setHidden(False)
             self.plotter_widget_run_func = self.plotter_widget.run
@@ -349,7 +276,6 @@ class UmapToolQt(QWidget):
         self.load_umap_tool.set_new_label_layer_name("UMAP Refined")
         worker = self.load_umap_tool.start_umap_worker(tmp_umap_pth)
         worker.start()
-        #worker.finished.connect(lambda x,y: self.viewer.window._qt_window.setEnabled(True))
 
 
     def show_targets_callback(self, future: futures.Future):
@@ -388,55 +314,6 @@ class UmapToolQt(QWidget):
         self.viewer.window._qt_window.setEnabled(True)
         self.napari_update_umap(umap_embeddings, used_embeddings)
 
-    def reestimate_umap(self):
-        try:
-            print("Read clusters")
-            clusters = self.plotter_widget.layer_select.value.features['MANUAL_CLUSTER_ID']
-            if not np.any(clusters>0):
-                raise KeyError
-        except KeyError:
-            notifications.show_info(f"No cluster selected. Can't refine.")
-            return
-
-        def get_embedding_path(pth: str) -> str:
-            '''
-            Checks if the embedding path exists. If it does not exist, it opens a file selection dialogue. Otherwise it returns the path.
-            '''
-            if not os.path.exists(pth):
-                msg = QMessageBox()
-                msg.setIcon(QMessageBox.Critical)
-                msg.setWindowTitle("Can't open embedding file")
-                msg.setText("Can't open embedding file")
-                msg.setInformativeText("Embedding path in metadata data (see below) does not exist or can't be accesst. Please click OK and select the path to the embedding file.")
-                msg.setDetailedText(pth)
-                msg.setStandardButtons(QMessageBox.Ok)
-                msg.exec_()
-                pth = QFileDialog.getOpenFileName(self, 'Open embedding file',
-                                                    os.getcwd(),
-                                                    "Embedding file (*.temb)")[0]
-
-            return pth
-
-        print("Read embeddings")
-        emb_pth = get_embedding_path(self.plotter_widget.layer_select.value.metadata['tomotwin']['embeddings_path'])
-
-
-        if emb_pth == "":
-            print("No path selected.")
-            return
-
-        self.plotter_widget.layer_select.value.metadata['tomotwin']['embeddings_path'] = emb_pth
-        embeddings = pd.read_pickle(emb_pth)
-
-        self.progressBar.setHidden(False)
-        self.progressBar.set_label_text("Recalculate umap")
-
-        # this workaround using signal is necessary, as "add_done_callback" starts the method
-        # in a separate thread, but to change Qt elements, it must be run in the same thread as the main program.
-        ppe = futures.ProcessPoolExecutor(max_workers=1)
-        f= ppe.submit(UmapRefiner.refine, clusters,embeddings)
-        ppe.shutdown(wait=False)
-        f.add_done_callback(self.refinement_done.emit)
 
 
 
